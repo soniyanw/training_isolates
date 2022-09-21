c992c5dc7da6bc6b58d12b84a41892d214a394f0
soniyanw <soniya@nativewit.in>
2022-09-21 22:10:49 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:isolate';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+double counter = 0;
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircularProgressIndicator(), //to show the addition of a very long number is done without a glitch in the app //in a seperate isolate
+            SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final receivePort = ReceivePort();
+          await Isolate.spawn(incrementCounter, receivePort.sendPort);
+          receivePort.listen((message) {
+            counter += message;
+            print(message);
+          });
+          setState(() {});
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+void incrementCounter(SendPort sendPort) {
+  int c = 0;
+  for (int i = 1; i <= 10000000; i++) {
+    c++;
+  }
+  sendPort.send(c);
+}
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/soniyanw/training_isolates.git
+git push -u origin main
